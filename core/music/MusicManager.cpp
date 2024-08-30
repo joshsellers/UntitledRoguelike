b68378c0aacdec909f2f1d9dd0c6040e5b60e6f5
joshsellers <joshsellers7.5@gmail.com>
2024-08-29 20:54:15 -0500

    Fix MusicManager, got wiped from previous merge

--- core/music/MusicManager.cpp
+++ core/music/MusicManager.cpp
@@ -3,7 +3,20 @@
 #include "../SoundManager.h"
 
 void MusicManager::start() {
-    _isHalted = false; 
+    _isHalted = false;
+
+    for (const auto& songName : SoundManager::musicNames) {
+        if (stringStartsWith(songName, "mainmenu")) {
+            _mainMenuSongs.push_back(songName);
+        } else if (stringStartsWith(songName, "cooldown")) {
+            _cooldownSongs.push_back(songName);
+        } else if (stringStartsWith(songName, "wave")) {
+            _cooldownSongs.push_back(songName);
+        }
+    }
+
+    std::thread musicThread(MusicManager::run);
+    musicThread.detach();
 }
 
 void MusicManager::shutdown() {
@@ -19,5 +32,42 @@ MUSIC_SITUTAION MusicManager::getSituation() {
 }
 
 void MusicManager::run() {
+    while (!_isHalted) {
+        std::this_thread::sleep_for(std::chrono::milliseconds((int)(500.f)));
 
+        if (_situation != _lastSituation) {
+            _lastSituation = _situation;
+            SoundManager::stopMusic();
+
+            switch (_situation) {
+                case MUSIC_SITUTAION::MAIN_MENU:
+                    if (_mainMenuSongs.size() > 1) {
+                        SoundManager::playSong(_mainMenuSongs.at(randomInt(0, _mainMenuSongs.size() - 1)));
+                    } else if (_mainMenuSongs.size() == 1) {
+                        SoundManager::playSong(_mainMenuSongs.at(0));
+                    }
+                    break;
+                case MUSIC_SITUTAION::COOLDOWN:
+                    if (_cooldownSongs.size() > 1) {
+                        SoundManager::playSong(_cooldownSongs.at(randomInt(0, _cooldownSongs.size() - 1)));
+                    } else if (_cooldownSongs.size() == 1) {
+                        SoundManager::playSong(_cooldownSongs.at(0));
+                    }
+                    break;
+                case MUSIC_SITUTAION::WAVE:
+                    if (_waveSongs.size() > 1) {
+                        SoundManager::playSong(_waveSongs.at(randomInt(0, _waveSongs.size() - 1)));
+                    } else if (_waveSongs.size() == 1) {
+                        SoundManager::playSong(_waveSongs.at(0));
+                    }
+                    break;
+                case MUSIC_SITUTAION::BOSS:
+
+                    break;
+                case MUSIC_SITUTAION::SHOP:
+
+                    break;
+            }
+        }
+    }
 }
