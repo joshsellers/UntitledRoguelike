48ce3db89eaccc344ef8fccb65a520e4876226a1
joshsellers <joshsellers7.5@gmail.com>
2024-11-10 01:20:54 -0600

    Increase shop spawn rate in hard mode

--- world/World.cpp
+++ world/World.cpp
@@ -757,7 +757,7 @@ void World::generateChunkScatters(Chunk& chunk) {
     int chY = chunk.pos.y;
 
     constexpr int altarSpawnRate = 27000;
-    const int shopSpawnRate = (HARD_MODE_ENABLED ? 23500 : 20000);
+    const int shopSpawnRate = (HARD_MODE_ENABLED ? 23000 : 20000);
     constexpr int grassSpawnRate = 25;
     constexpr int smallTreeSpawnRate = 187;
     constexpr int cactusSpawnRate = 1000;
