c8ba05fadaebe23cbb95a9ad45328522bf387ee0
joshsellers <joshsellers7.5@gmail.com>
2024-11-28 19:28:16 -0600

    Change increased enemy spawn rate to increase with displayed wave number

--- world/World.cpp
+++ world/World.cpp
@@ -297,7 +297,7 @@ void World::spawnMobs() {
 }
 
 void World::spawnEnemies() {
-    const float MOB_SPAWN_RATE_SECONDS = std::max(0.05f, (500.f - _waveCounter * 10.f) / 1000.f);
+    const float MOB_SPAWN_RATE_SECONDS = std::max(0.05f, (500.f - _currentWaveNumber * 10.f) / 1000.f);
     constexpr int MOB_SPAWN_CHANCE = 5;
     constexpr float MIN_DIST = 250.f;
     constexpr int PACK_SPREAD = 20;
