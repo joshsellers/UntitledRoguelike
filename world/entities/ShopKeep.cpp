bed1885002e13d6ef517589abe17173b5238c575
joshsellers <joshsellers7.5@gmail.com>
2025-07-13 03:21:14 -0500

    Don't spawn bloat jewel in shop if player already has it

--- world/entities/ShopKeep.cpp
+++ world/entities/ShopKeep.cpp
@@ -89,6 +89,7 @@ void ShopKeep::initInventory(bool visited) {
                 else if (item->getId() == Item::COIN_MAGNET.getId() && getWorld()->getPlayer()->getCoinMagnetCount() == 12) continue;
                 if (item->getId() == Item::getIdFromName("Rebound Jewel") && AbilityManager::playerHasAbility(Ability::BOUNCING_PROJECTILES.getId())) continue;
                 else if (item->getId() == Item::getIdFromName("Burst Jewel") && AbilityManager::playerHasAbility(Ability::EXPLOSIVE_ROUNDS.getId())) continue;
+                else if (item->getId() == Item::getIdFromName("Bloat Jewel") && AbilityManager::playerHasAbility(Ability::BIG_BULLETS.getId())) continue;
                 else if (item->getId() == Item::getIdFromName("Map") && getWorld()->getPlayer()->getInventory().hasItem(Item::getIdFromName("Map"))) continue;
 
                 availableItems.push_back(item->getId());
