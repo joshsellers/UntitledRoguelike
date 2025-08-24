a7d39e8bf82521f6a3791d7825a5850b63a2d0c1
joshsellers <joshsellers7.5@gmail.com>
2025-08-24 16:15:13 -0500

    Don't spawn altars in mountains unless hardmode untouchable is unlocked

--- world/World.cpp
+++ world/World.cpp
@@ -882,7 +882,7 @@ void World::generateChunkScatters(Chunk& chunk) {
                 }
             }
 
-            if (!spawnedAltarThisChunk && (terrainType == TERRAIN_TYPE::MOUNTAIN_HIGH)) {
+            if (!spawnedAltarThisChunk && (terrainType == TERRAIN_TYPE::MOUNTAIN_HIGH) && LocalAchievementManager::isUnlocked(HARDMODE_UNTOUCHABLE)) {
                 const sf::Vector2f pos = sf::Vector2f(x, y);
                 boost::random::uniform_int_distribution<> altarDist(0, altarSpawnRate * (terrainType == TERRAIN_TYPE::TUNDRA ? 18 : 1));
                 if (altarDist(gen) == 0 && !isPropDestroyedAt(pos)) {
